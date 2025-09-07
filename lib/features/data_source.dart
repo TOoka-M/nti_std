402619656ab0dcc452cf7d3a9ff1c74fcd36d984
TOoka-M <tqyshkr@gmail.com>
2025-09-07 16:15:30 +0300

    Toka Medhatt message

--- lib/features/data_source.dart
+++ lib/features/data_source.dart
@@ -13,7 +13,7 @@ final List<Person> persons = [
   Person(
     name: "Sara",
     message:
-        "In today’s fast-paced digital world, technology continues to shape the way we live, work, and connect with each other. From the moment we wake up and check our phones, to the late hours when we browse the internet before going to sleep, technology surrounds us and influences almost every aspect of our daily lives. It provides us with tools that make communication faster, access to information easier, and entertainment more interactive than ever before. However, this convenience also comes with challenges, such as the risk of distraction, the spread of misinformation, and the growing concerns around privacy and data security.",
+        "In today's fast-paced digital world, technology continues to shape the way we live, work, and connect with each other. From the moment we wake up and check our phones, to the late hours when we browse the internet before going to sleep, technology surrounds us and influences almost every aspect of our daily lives. It provides us with tools that make communication faster, access to information easier, and entertainment more interactive than ever before. However, this convenience also comes with challenges, such as the risk of distraction, the spread of misinformation, and the growing concerns around privacy and data security.",
     screenColor: ColorPallete.deepOrange,
   ),
   Person(
