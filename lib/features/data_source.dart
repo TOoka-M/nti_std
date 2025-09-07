1f00b992cf36be89fc9907091794ff0e75b8e9bd
TOoka-M <tqyshkr@gmail.com>
2025-09-07 15:36:37 +0300

    Toka-MedhatMessage

--- lib/features/data_source.dart
+++ lib/features/data_source.dart
@@ -16,6 +16,34 @@ final List<Person> persons = [
         "In today’s fast-paced digital world, technology continues to shape the way we live, work, and connect with each other. From the moment we wake up and check our phones, to the late hours when we browse the internet before going to sleep, technology surrounds us and influences almost every aspect of our daily lives. It provides us with tools that make communication faster, access to information easier, and entertainment more interactive than ever before. However, this convenience also comes with challenges, such as the risk of distraction, the spread of misinformation, and the growing concerns around privacy and data security.",
     screenColor: ColorPallete.deepOrange,
   ),
-  Person(name: "mahmoud", message: "I am here", image: "download".jpg , screenColor: ColorPallete.cyan ),
-  Person(name: "Mahmoud saeed", message: "dfasdf sdfasd ", image: "download".jpg, screenColor: ColorPallete.lightBlue),
+  Person(
+    name: "mahmoud",
+    message: "I am here",
+    image: "download".jpg,
+    screenColor: ColorPallete.cyan,
+  ),
+  Person(
+    name: "Mahmoud saeed",
+    message: "dfasdf sdfasd ",
+    image: "download".jpg,
+    screenColor: ColorPallete.lightBlue,
+  ),
+  Person(
+    name: "Toka Medhat",
+    message: '''He who does not thank people does not thank God
+
+    This is the least thing we can do to express our thanks and gratitude to you.
+
+I am truly very grateful for everything you did with us throughout the training period.
+Your effort, patience, and care to deliver the information to us in a simple and clear way made a big difference for us, and also made the experience very enjoyable and beneficial.
+You were not only transferring knowledge to us, but you were also always encouraging us to love the field and continue in it.
+
+Really Your advices made a big difference to me.
+
+May God bless you, your life, and your efforts, increase you from His grace and bounty, and always make you a reason for spreading knowledge to others
+
+
+''',
+    screenColor: ColorPallete.purple,
+  ),
 ];
